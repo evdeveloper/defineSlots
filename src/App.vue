7ff92f49a58e462a888e86c938ca7b9f5784b0fd
«Evgeny» <mushnikovz@mail.ru>
2023-10-29 22:09:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import DaisyUIModal from "./components/DaisyUIModal.vue";
+</script>
+
+<template>
+  <DaisyUIModal>
+    <!-- <template #title>Modal title</template> -->
+    <template #default>Default slot</template>
+    <template #text="{short}">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est sapiente</p>
+      <p v-if="!short">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est sapiente</p>
+    </template>
+  </DaisyUIModal>
+  
+</template>
---
+++ src/components/DaisyUIModal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  const slots = defineSlots<{
+    title?: (props: { msg?: string }) => any
+    text?: (props: { short: boolean }) => any
+    default?:(prors: {}) => any
+  }>()
+</script>
+
+
+<template>
+  <dialog id="my_modal_1" class="modal" open>
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">
+      <slot name="title" />
+      <p v-if="!slots.title">Some text</p>
+    </h3>
+    <slot name="text" :short="true" />
+    <slot></slot>
+    <div class="modal-action">
+      <form method="dialog">
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+</template>
